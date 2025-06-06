06aa64db253db78a2101d7c90da22e368b68324c
Longistamina <dophamthelong03@gmail.com>
2025-06-06 16:47:55 +0700

    modified somethings

--- Fedora_wallpapers/change_wallpapers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WALLPAPER_DIR="/home/longdpt/Pictures/Fedora_wallpapers"
-PIC=$(find "$WALLPAPER_DIR" -type f | shuf -n1)
-gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
-
